af1c2fee41f09f1eebe9ea4efc6379553370ccf6
Ullash Bhattacharjee(Turjo) <53423899+turjo997@users.noreply.github.com>
2022-11-02 21:11:23 +0600

    Create Contains Duplicate.cpp

---
+++ LeetCode/Contains Duplicate.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        set<int> s;
+
+        for(auto i : nums){
+           s.insert(i);
+        }
+
+        if(s.size() == nums.size()){
+            return false;
+        }else{
+            return true;
+        }
+    }
+};
